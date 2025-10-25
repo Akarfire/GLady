44d378abd41a63e1347d16807c033d692af359d9
K <Akarfire@gmail.com>
2025-10-25 13:57:26 +0300

    [SIDE DEV] Console Control Panel crashing fix

--- ControlPanel/GLady_ConsoleControlPanel.py
+++ ControlPanel/GLady_ConsoleControlPanel.py
@@ -5,18 +5,22 @@ port = 22222
 
 while True:
     
-    controlSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    controlSocket.connect((host, port))
-    
-    print(f"Connected to GLady control server!")
-    
-    # Main loop
-    while True:
-       
-        command = input()
+    try:
+        controlSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        controlSocket.connect((host, port))
+        
+        print(f"Connected to GLady control server!")
+        
+        # Main loop
+        while True:
+        
+            command = input()
 
-        controlSocket.sendall(command.encode("utf-8"))
-        
-        response = controlSocket.recv(1024)
-        
-        print(response.decode("utf-8"))
+            controlSocket.sendall(command.encode("utf-8"))
+            
+            response = controlSocket.recv(1024)
+            
+            print(response.decode("utf-8"))
+            
+    except Exception as e:
+        print(f"CONNECTION ERROR: {str(e)}\n")
