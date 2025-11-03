fd00953f7b0f1f674f56aba385c997bdf17c2777
K <Akarfire@gmail.com>
2025-11-04 01:57:41 +0300

    [PLUGIN DEV] TwitchChatReader event fix

--- Plugins/TwitchChatReader/TwitchChatReader.py
+++ Plugins/TwitchChatReader/TwitchChatReader.py
@@ -65,7 +65,9 @@ class TwitchChatReader(PluginAPI.Plugin):
         self.queueAccess.acquire()
         
         while not self.messageQueue.empty():
-            self.core.communicationBus.init_event(self.options["OnMessageFetchedEventName"], self.pluginName, set(), self.messageQueue.get())
+            
+            event = PluginAPI.Event(self.options["OnMessageFetchedEventName"], self.pluginName, set(), self.messageQueue.get())
+            self.core.communicationBus.init_event(event)
             
         self.queueAccess.release()
         
